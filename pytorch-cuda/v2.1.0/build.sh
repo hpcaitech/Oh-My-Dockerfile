f4dd259e3b6e1e7d0b68a2d8ae75d7bfc78a0d41
FrankLeeeee <somerlee.9@gmail.com>
2024-01-10 07:37:39 +0000

    updated torch 2.1 and cuda 12

--- pytorch-cuda/v2.1.0/build.sh
+++ pytorch-cuda/v2.1.0/build.sh
@@ -1,1 +1,1 @@
-docker build -t hpcaitech/pytorch-cuda:2.1.0-11.8.0 .
+docker build --build-arg http_proxy=http://172.17.0.1:7890 --build-arg http_proxy=http://172.17.0.1:7890  -t hpcaitech/pytorch-cuda:2.1.0-12.1.0 .
